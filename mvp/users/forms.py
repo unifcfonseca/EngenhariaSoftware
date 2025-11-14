8c615775a5602d3e251f8b210ee25c1521364524
unifcfonseca <163344346+unifcfonseca@users.noreply.github.com>
2025-11-14 18:10:01 -0300

    Add files via upload

--- mvp/users/forms.py
+++ mvp/users/forms.py
@@ -6,6 +6,18 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'user_type', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'password1' in self.fields:
+            self.fields['password1'].help_text = ''
+        if 'password2' in self.fields:
+            self.fields['password2'].help_text = ''
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
--- mvp/users/views.py
+++ mvp/users/views.py
@@ -1,21 +1,65 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .forms import CustomUserCreationForm
 from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from .forms import CustomUserCreationForm
+
+User = get_user_model()
 
 
 def register(request):
+    print("=== ENTROU NA VIEW REGISTER ===", flush=True)
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Conta criada com sucesso! Faça login.")
+            user = form.save(commit=False)
+            print(user.username, "Antes de user.is_active=False:", user.is_active, flush=True)
+            user.is_active = False
+            user.save()
+            print(user.username, "Depois de salvar:", user.is_active, flush=True)
+
+            current_site = get_current_site(request)
+            subject = 'Ative sua conta'
+            message = render_to_string('users/email_verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            email = EmailMessage(subject, message, to=[user.email])
+            email.content_subtype = "html"
+            email.send()
+
+            messages.info(request, "Verifique seu e-mail para ativar sua conta.")
             return redirect('login')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    print("=== ENTROU NA VIEW ACTIVATE ===",token, flush=True)
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if (user and default_token_generator.check_token(user, token)):
+        user.is_active = True
+        print("=== ENTROU NA VIEW ACTIVATE e check token ===", user.username, flush=True)
+        user.save()
+        messages.success(request, 'Conta ativada com sucesso! Você já pode fazer login.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Link de ativação inválido ou expirado.')
+        return redirect('register')
 
 
 def user_login(request):
@@ -26,11 +70,14 @@ def user_login(request):
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
-                login(request, user)
-                messages.success(request, f"Bem-vindo, {username}!")
-                # Redireciona para a página inicial após login
-                return redirect('/')
-        messages.error(request, "Usuário ou senha inválidos.")
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, f"Bem-vindo, {username}!")
+                    return redirect('/')
+                else:
+                    messages.warning(request, "Sua conta ainda não foi verificada. Verifique seu e-mail.")
+                    return redirect('login')
+        messages.error(request, "Usuário ou senha inválidos. Verifique se recebeu um email de verificação de conta.")
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})

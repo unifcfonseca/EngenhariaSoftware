a9802f54bdd1947f51ffc4de070e706c856c3887
unifcfonseca <163344346+unifcfonseca@users.noreply.github.com>
2025-11-14 18:11:18 -0300

    Add files via upload

--- mvp/videos/forms.py
+++ mvp/videos/forms.py
@@ -4,4 +4,19 @@ from .models import Video
 class VideoForm(forms.ModelForm):
     class Meta:
         model = Video
-        fields = ['title', 'description', 'subject', 'thumbnail', 'video_file']
+        fields = ['title', 'description', 'subject', 'thumbnail', 'thumbnail_url', 'video_file', 'video_url']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        video_file = cleaned_data.get('video_file')
+        video_url = cleaned_data.get('video_url')
+        thumb_file = cleaned_data.get('thumbnail')
+        thumb_url = cleaned_data.get('thumbnail_url')
+
+        if not thumb_file and not thumb_url:
+            raise forms.ValidationError("Envie uma miniatura ou insira o link da miniatura.")
+
+        if not video_file and not video_url:
+            raise forms.ValidationError("Envie um vídeo ou insira o link do vídeo.")
+
+        return cleaned_data
